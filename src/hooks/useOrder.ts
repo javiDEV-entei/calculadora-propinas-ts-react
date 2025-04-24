77abc1ea8cc7830e10bd162a399cfc6bda5f7453
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-04-24 16:13:59 -0300

    pasando las ordenes al state y ajustando detalles
    se agrego codigo para pasar las ordenes al state y verificaciones que evitan duplicados

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { MenuItem, OrderItem } from "../types"
 
 
 export default function useOrder() {
@@ -7,8 +7,20 @@ export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
 
-        const addItem = () =>{
-            console.log("agregando..");
+        const addItem = (item:MenuItem) =>{
+            const itemExists = order.find(orderItem => orderItem.id === item.id)
+            if (itemExists) {
+                const updatedOrder =order.map(orderItem => orderItem.id === item.id?
+                    {...orderItem, quantity: orderItem.quantity + 1}: orderItem)
+                
+                setOrder(updatedOrder)
+                
+            }else{
+
+            const newItem: OrderItem ={...item, quantity: 1}
+            setOrder([...order, newItem])
+            }
+            
             
         }
 
